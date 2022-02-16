876eddc191162247ce15c32a588c75864261e699
Baris Simsek <baris.simsek@gmail.com>
2022-02-15 23:55:15 -0800

    remove unnecessary var

--- config_test.go
+++ config_test.go
@@ -9,8 +9,7 @@ import (
 func TestConfig(t *testing.T) {
 	want := []byte(`{"targets":[{"ip":"10.0.0.10","port":"8080"}]}`)
 
-	c := Parse("test_data/config.json")
-	got, _ := json.Marshal(c)
+	got, _ := json.Marshal(Parse("test_data/config.json"))
 
 	if !bytes.Equal(got, want) {
 		t.Errorf("got %q, wanted %q", got, want)
